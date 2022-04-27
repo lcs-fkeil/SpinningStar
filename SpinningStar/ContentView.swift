8e02dc2c4977720c799c83b3ac8898732d4fcac4
Franka Keil <fkeil@lcs.on.ca>
2022-04-27 11:34:54 -0400

    Star now rotates when it is clicked.

--- SpinningStar/ContentView.swift
+++ SpinningStar/ContentView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    // MARK: Stored properties
+    
+    // 1. Initial state of rotation for the star
+    @State var starRotation = 0.0
+    
+    
+    // MARK: Computed properties
+    
     var body: some View {
         Image(systemName: "star")
             .resizable()
             .frame(width: 40, height: 40)
             .foregroundColor(.yellow)
+            .rotation3DEffect(.degrees(starRotation),
+                              axis: (x: 0, y: 1, z: 0))
+            .animation(.default)
+            // 2. Trigger the state change with a tap
+            .onTapGesture {
+                // 3. Rotate the star twice (change rotation state)
+                starRotation += 360.0 * 2
+            }
     }
 }
 
