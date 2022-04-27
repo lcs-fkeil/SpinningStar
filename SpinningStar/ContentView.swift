83f2842f59107fc91827376dcf3ca5aefd598d4c
Franka Keil <fkeil@lcs.on.ca>
2022-04-27 11:55:02 -0400

    The unfilled star disappears and the filled star appears. And vice versa to reverse the animation.

--- SpinningStar/ContentView.swift
+++ SpinningStar/ContentView.swift
@@ -18,32 +18,77 @@ struct ContentView: View {
     // 1. Initial opacity fo unfilled star
     @State var unfilledStarOpacity = 1.0
     
+    // 1. Initial opacity fo filled star
+    @State var filledStarOpacity = 0.0
+    
     
     // MARK: Computed properties
     
     var body: some View {
-        Image(systemName: "star")
-            .resizable()
-            .frame(width: 40, height: 40)
-            .foregroundColor(.yellow)
-            .rotation3DEffect(.degrees(starRotation),
-                              axis: (x: 0, y: 1, z: 0))
-            .opacity(unfilledStarOpacity)
         
-            // 2. Trigger the state change with a tap
-            .onTapGesture {
-                withAnimation(
-                    Animation
-                        .easeInOut(duration: 2.0)
-                ){
-                    // 3. Rotate the star twice (change rotation state)
-                    starRotation += 360.0 * 2
+        
+        ZStack{
+            
+            // Filled star
+            Image(systemName: "star.fill")
+                .resizable()
+                .frame(width: 40, height: 40)
+                .foregroundColor(.yellow)
+                .rotation3DEffect(.degrees(starRotation),
+                                  axis: (x: 0, y: 1, z: 0))
+                .opacity(filledStarOpacity)
+            
+                // 2. Trigger the state change with a tap
+                .onTapGesture {
+                    withAnimation(
+                        Animation
+                            .easeInOut(duration: 2.0)
+                    ){
+                        // 3. Rotate the star twice (change rotation state)
+                        starRotation += 360.0 * 2
+                        
+                        // 3. Desired end opacity for unfilled star
+                        unfilledStarOpacity = 1.0
+                        
+                        // 3. Desired end opacity for filled star
+                        filledStarOpacity = 0.0
                     
-                    // 3. Desired end opacity for unfilled star
-                    unfilledStarOpacity = 0.0
+                    }
                 }
-                
-            }
+            
+            
+            
+            
+            // Unfilled star
+            Image(systemName: "star")
+                .resizable()
+                .frame(width: 40, height: 40)
+                .foregroundColor(.yellow)
+                .rotation3DEffect(.degrees(starRotation),
+                                  axis: (x: 0, y: 1, z: 0))
+                .opacity(unfilledStarOpacity)
+
+            // 2. Trigger the state change with a tap
+                .onTapGesture {
+                    withAnimation(
+                        Animation
+                            .easeInOut(duration: 2.0)
+                    ){
+                        // 3. Rotate the star twice (change rotation state)
+                        starRotation += 360.0 * 2
+
+                        // 3. Desired end opacity for unfilled star
+                        unfilledStarOpacity = 0.0
+                        
+                        // 3. Desired end opacity for filled star
+                        filledStarOpacity = 1.0
+                    }
+
+                }
+            
+            
+            
+        }
     }
 }
 
